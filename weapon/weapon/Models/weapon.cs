6de052d459117214bae2937337b6fdf3b4a0596f
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-03-15 10:59:43 +0400

    add

---
+++ weapon/weapon/Models/weapon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace weapon.Models
+{
+    class weapon
+    {
+        public int bulletCapacity ;
+        public int numberOfBullet;
+        public double secondsOfCombDischarge;
+        public bool sootingMode;
+        public weapon(int bulletCapacity, int numberOfBullet, double secondsOfCombDischarge, bool sootingMode)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.numberOfBullet = numberOfBullet;
+            this.secondsOfCombDischarge = secondsOfCombDischarge;
+            this.sootingMode = sootingMode;
+        }
+
+        public void Shoot()
+        {
+            if (numberOfBullet>0)
+            {
+                numberOfBullet = numberOfBullet - 1;
+                Console.WriteLine($"pew pew {numberOfBullet}");
+            }
+            else
+                Console.WriteLine("There are no bullets in the comb");
+        }
+
+        public void Fire()
+        {
+            if (bulletCapacity>0)
+            {
+                secondsOfCombDischarge = numberOfBullet * secondsOfCombDischarge;
+                Console.WriteLine($"seconds of comb discharge:{secondsOfCombDischarge}");
+            }
+            else
+                Console.WriteLine("There are no bullets in the comb");
+        }
+
+        public void GetRemainBulletCount()
+        {
+            if (bulletCapacity>numberOfBullet)
+            {
+                bulletCapacity = bulletCapacity - numberOfBullet;
+                Console.WriteLine($"The number of bullets needed to fill the comb:{bulletCapacity}");
+            }
+            else
+                Console.WriteLine("Enter the numbers correctly");
+            
+        }
+         
+        public void Reload()
+        {
+            if (bulletCapacity>numberOfBullet)
+            {
+                int Empty = bulletCapacity - numberOfBullet;
+                bulletCapacity = Empty + numberOfBullet;
+                Console.WriteLine("The comb is full");
+            }
+           
+
+        }
+         
+        public void ChangeFireMode()
+        {
+            if (sootingMode == true )
+            {
+
+                Console.WriteLine("Single shot was fired");
+            }
+            else if (sootingMode == false)
+            {
+                Console.WriteLine("Automatic firing");
+            }
+        }
+         
+        public void Getinfo()
+        {
+            Console.WriteLine("1 Shoot ");
+            Console.WriteLine("2 Fire ");
+            Console.WriteLine("3 GetRemainBulletCount ");
+            Console.WriteLine("4 Reload ");
+            Console.WriteLine("5 ChangeFireMode ");
+        }
+            
+            
+
+
+
+    }   
+
+
+}
---
+++ weapon/weapon/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using weapon.Models;
+
+namespace weapon.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Note the capacity of the comb:");
+            int bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Record the number of bullets in the comb:");
+            int numberOfBullet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Record the seconds of a bullet's release:");
+            double secondsOfCombDischarge = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Note the type of shot:");
+            bool sootingMode = Convert.ToBoolean(Console.ReadLine());
+
+            weapon W1 = new weapon(bulletCapacity,numberOfBullet,secondsOfCombDischarge,sootingMode);
+
+            int key = 0;
+            do
+            {
+                Console.WriteLine("Enter the number: ");
+                
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        W1.ChangeFireMode();
+                        break;
+                    case 2:
+                        W1.Shoot();
+                        break;
+                    case 3:
+                        W1.Fire();
+                        break;
+                    case 4:
+                        W1.GetRemainBulletCount();
+                        break;
+                    case 5:
+                        W1.Reload();
+                        break;
+                    case 6:
+                        Console.WriteLine("END");
+                        break;
+                    case 0:
+                        W1.Getinfo();
+                        break;
+                    default:
+                        Console.WriteLine("Entered incorrectly");
+                        break;
+                }
+            } while (key != 6);
+
+
+        }
+    }
+}
